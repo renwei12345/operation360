dacd59f272ebf9446d532e18c18a39fb68e46f97
renwei12345 <ele_ren@163.com>
2014-07-28 09:03:05 +0800

    将日志文件按天存储，来限定文件大小；文件名是:日期.txt

--- application/sysinfo_recorder.pl
+++ application/sysinfo_recorder.pl
@@ -3,6 +3,9 @@
 while(1){
 	my %result;					#键为系统时间，值为cpu状态字符串
 	my $t=time();
+	($sec,$min,$hour,$day,$mon,$year,$wday,$yday,$isdst)=localtime(time());
+	$mon += 1;
+	$year += 1900;
 	my @list =`top -bcn 1`;
 	print @list;
 	my $cpuinfo;
@@ -15,8 +18,7 @@ while(1){
 	}
 	print "cpu :",$cpuinfo, "\n";
 	$result{$t} = $cpuinfo;
-	$index ++;
-	my $filename = 'report.txt';
+	my $filename = "$day,$mon,$year.txt";				#配置日志文件名，设置为每天一个文件，以日期作为文件名
 	open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
 	say $fh %result;
 	close $fh;
